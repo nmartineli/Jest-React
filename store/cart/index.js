e5e63fc738cbf74b5d275b1972c0773d559132bb
nataliamartineli <natalia.martineli@ilia.digital>
2023-02-10 18:37:28 -0300

    Store refactor

--- store/cart/index.js
+++ store/cart/index.js
@@ -1,4 +1,7 @@
 import create from 'zustand';
+import produce from 'immer';
+
+//immer -> pega um objeto, faz a modificação, cria uma cópia com tudo o que já existia e adiciona a sua modificação
 
 const initialState = {
   open: false,
@@ -12,18 +15,31 @@ const addProduct = (store, product) => {
   return [...store.state.products, product];
 };
 
-export const useCartStore = create((set) => ({
-  state: {
-    ...initialState,
-  },
-  actions: {
-    toggle: () =>
-      set((store) => ({ state: { ...store.state, open: !store.state.open } })),
-    reset: () => set((store) => ({ state: { ...initialState } })),
+export const useCartStore = create((set) => {
+  const setState = (fn) => set(produce(fn));
 
-    add: (product) =>
-      set((store) => ({
-        state: { open: true, products: addProduct(store, product) },
-      })),
-  },
-}));
+  return {
+    state: {
+      ...initialState,
+    },
+    actions: {
+      toggle() {
+        setState(({ state }) => {
+          state.open = !state.open;
+        });
+      },
+      reset() {
+        setState((store) => {
+          store.state = initialState;
+        });
+      },
+      add(product) {
+        setState(({ state }) => {
+          if (!state.products.includes(product)) {
+            state.products.push(product);
+          }
+        });
+      },
+    },
+  };
+});
